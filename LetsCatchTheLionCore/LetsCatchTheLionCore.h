49ba199ef03f79695eac4bd6a4caa79ea16aed90
Łukasz Gryl <lukasz.gryl@gmail.com>
2020-12-31 09:27:59 +0100

    Initial commit

---
+++ LetsCatchTheLionCore/LetsCatchTheLionCore.h
@@ -0,0 +1,13 @@
+// Created 31/12/2020
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LetsCatchTheLionCore.
+FOUNDATION_EXPORT double LetsCatchTheLionCoreVersionNumber;
+
+//! Project version string for LetsCatchTheLionCore.
+FOUNDATION_EXPORT const unsigned char LetsCatchTheLionCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LetsCatchTheLionCore/PublicHeader.h>
+
+
